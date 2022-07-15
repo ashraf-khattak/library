dfb6cdcecf80dadf207ed176a66c70a972ddf06a
Muhammad Ashraf Khan Khattak <ashes.developer@gmail.com>
2022-07-16 04:58:42 +0500

    #UpdatedSaterday

--- src/App.tsx
+++ src/App.tsx
@@ -1,286 +1,315 @@
-import * as React from "react";
-import Tabs from "@mui/material/Tabs";
+import React from "react";
+import "./App.scss";
 import {
   Box,
+  Grid,
+  Typography,
   Card,
   CardContent,
-  Grid,
-  CardActions,
-  Tab,
-  Typography,
+  Avatar,
 } from "@mui/material";
 import { Container } from "@mui/system";
-import TextField from "@mui/material/TextField";
-interface TabPanelProps {
-  children?: React.ReactNode;
-  index: number;
-  value: number;
-}
+import imgTwo from "../src/Assests/images/imgTwo.png";
 
-function TabPanel(props: TabPanelProps) {
-  const { children, value, index, ...other } = props;
-
+function App() {
   return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`vertical-tabpanel-${index}`}
-      aria-labelledby={`vertical-tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box>
-          <Typography>{children}</Typography>
+    <div>
+      <Container maxWidth="xl">
+        <Box color="green" fontWeight={800} mt={6}>
+          <h2>Welcome Panel Member!</h2>
         </Box>
-      )}
+        <Grid container spacing={2} direction="row">
+          <Grid item md={2} sm={6} xs={12}>
+            <Box bgcolor="red" textAlign="center">
+              <Card >
+                <CardContent>
+                <Box alignContent="center"><Avatar
+                    alt="Remy Sharp"
+                    src={imgTwo}
+                    
+                    sx={{ width: 90, height: 90 }}
+                    
+                  /></Box>
+                  <Typography
+                    sx={{ fontSize: 14 }}
+                    color="text.secondary"
+                    gutterBottom
+                  >
+                    <Box color="green" fontWeight={800} textAlign="center">
+                      <h2>Steven Tyler</h2>
+                    </Box>
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Box>
+          </Grid>
+          <Grid item md={3} sm={6} xs={12}>
+            <Box bgcolor="red">1</Box>
+          </Grid>
+          <Grid item md={4} sm={6} xs={12}>
+            <Box bgcolor="red">1</Box>
+          </Grid>
+          <Grid item md={3} sm={6} xs={12}>
+            <Box bgcolor="red">1</Box>
+          </Grid>
+        </Grid>
+      </Container>
     </div>
   );
 }
 
-function a11yProps(index: number) {
-  return {
-    id: `vertical-tab-${index}`,
-    "aria-controls": `vertical-tabpanel-${index}`,
-  };
-}
+export default App;
 
-export default function VerticalTabs() {
-  const [value, setValue] = React.useState(0);
+// import * as React from "react";
+// import Tabs from "@mui/material/Tabs";
+// import {
+//   Box,
+//   Card,
+//   CardContent,
+//   Grid,
+//   CardActions,
+//   Tab,
+//   Typography,
+// } from "@mui/material";
+// import { Container } from "@mui/system";
+// import TextField from "@mui/material/TextField";
+// interface TabPanelProps {
+//   children?: React.ReactNode;
+//   index: number;
+//   value: number;
+// }
 
-  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
-    setValue(newValue);
-  };
+// function TabPanel(props: TabPanelProps) {
+//   const { children, value, index, ...other } = props;
 
-  return (
-    <Container maxWidth="lg" sx={{ mt: 8 }}>
-      <h1 className='{margin="0px"}'>Hi Welcome</h1>
-      <h2>John David</h2>
-      <Grid>
-        <Box
-          sx={{
-            flexGrow: 1,
-            bgcolor: "background.paper",
-            display: "flex",
-            justifyContent: "left",
-          }}
-        >
-          <Card>
-            <Tabs
-              orientation="vertical"
-              variant="scrollable"
-              value={value}
-              onChange={handleChange}
-              aria-label="Vertical tabs example"
-              sx={{ borderRight: 1, borderColor: "divider" }}
-            >
-              <div className="text-start">
-              <Tab
-                style={{ color: "yellow" }}
-                label="Set Key Info"
-                {...a11yProps(0)}
-              /></div>
-              <Tab label="Manage Job Role" {...a11yProps(1)} />
-              <Tab
-                label="Staff Settings-----------------------------------"
-                {...a11yProps(2)}
-              />
-              <Tab
-                label="Define Bank Holiday---------------------------"
-                {...a11yProps(3)}
-              />
-              <Tab
-                label="Neutral Vendors----------------------------"
-                {...a11yProps(4)}
-              />
-              <Tab
-                label="Email Notification Settings-----------"
-                {...a11yProps(5)}
-              />
-              <Tab
-                label="Staff / Client Terms & Conditions----"
-                {...a11yProps(6)}
-              />
-              <Tab label="Agency Staff Agreement-----" {...a11yProps(7)} />
-              <Tab label="Staff Referral System-----" {...a11yProps(8)} />
-              <Tab
-                label="Reset Primary Email and Phone---------"
-                {...a11yProps(9)}
-              />
-              <Tab
-                label="Choose Your Week Start Day----------"
-                {...a11yProps(10)}
-              />
-              <Tab
-                label="Admin Document Wallet-----------"
-                {...a11yProps(11)}
-              />
-              <Tab label="Staff Document Wallet---------" {...a11yProps(12)} />
-              <Tab
-                label="Electronic Attendance Monitoring------"
-                {...a11yProps(13)}
-              />
-              <Tab label="Festival Day Greetings------" {...a11yProps(14)} />
-              <Tab
-                label="Roles and Rights---------------------------"
-                {...a11yProps(15)}
-              />
-            </Tabs>
-          </Card>
-
-          <CardContent
-            style={{ borderColor: "red", borderStyle: "solid" }}
-            sx={{
-              flexGrow: 1,
-              bgcolor: "background.paper",
-              display: "flex",
-              justifyContent: "left",
-            }}
-          >
-            <TabPanel value={value} index={0}>
-              <Box
-                component="form"
-                sx={{
-                  "& .MuiTextField-root": { m: 1, width: "25ch" },
-                }}
-                noValidate
-                autoComplete="off"
-              >
-                <div>
-                  <TextField
-                    error
-                    id="outlined-error"
-                    label="Error"
-                    defaultValue="Hello World"
-                  />
-                  <TextField
-                    error
-                    id="outlined-error-helper-text"
-                    label="Error"
-                    defaultValue="Hello World"
-                    helperText="Incorrect entry."
-                  />
-                </div>
-                <div>
-                  <TextField
-                    error
-                    id="filled-error"
-                    label="Error"
-                    defaultValue="Hello World"
-                    variant="filled"
-                  />
-                  <TextField
-                    error
-                    id="filled-error-helper-text"
-                    label="Error"
-                    defaultValue="Hello World"
-                    helperText="Incorrect entry."
-                    variant="filled"
-                  />
-                </div>
-                <div>
-                  <TextField
-                    error
-                    id="standard-error"
-                    label="Error"
-                    defaultValue="Hello World"
-                    variant="standard"
-                  />
-                  <TextField
-                    error
-                    id="standard-error-helper-text"
-                    label="Error"
-                    defaultValue="Hello World"
-                    helperText="Incorrect entry."
-                    variant="standard"
-                  />
-                </div>
-              </Box>
-            </TabPanel>
-            <TabPanel value={value} index={1}>
-              <Box> Item Two</Box>
-            </TabPanel>
-            <TabPanel value={value} index={2}>
-              Item Three
-            </TabPanel>
-            <TabPanel value={value} index={3}>
-              Item Four
-            </TabPanel>
-            <TabPanel value={value} index={4}>
-              Item Five
-            </TabPanel>
-            <TabPanel value={value} index={5}>
-              Item Six
-            </TabPanel>
-            <TabPanel value={value} index={6}>
-              Item Seven
-            </TabPanel>
-            <TabPanel value={value} index={7}>
-              Item Seven
-            </TabPanel>
-            <TabPanel value={value} index={8}>
-              Item Seven
-            </TabPanel>
-            <TabPanel value={value} index={9}>
-              Item Seven
-            </TabPanel>
-            <TabPanel value={value} index={10}>
-              Item Seven
-            </TabPanel>
-            <TabPanel value={value} index={11}>
-              Item Seven
-            </TabPanel>
-            <TabPanel value={value} index={12}>
-              Item Seven
-            </TabPanel>
-            <TabPanel value={value} index={13}>
-              Item Seven
-            </TabPanel>
-            <TabPanel value={value} index={14}>
-              Item Seven
-            </TabPanel>
-            <TabPanel value={value} index={15}>
-              Item Seven
-            </TabPanel>
-          </CardContent>
-        </Box>
-
-        <Card></Card>
-      </Grid>
-    </Container>
-  );
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import React from 'react';
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
 //   return (
-//     <div className="App">
-//      App
+//     <div
+//       role="tabpanel"
+//       hidden={value !== index}
+//       id={`vertical-tabpanel-${index}`}
+//       aria-labelledby={`vertical-tab-${index}`}
+//       {...other}
+//     >
+//       {value === index && (
+//         <Box>
+//           <Typography>{children}</Typography>
+//         </Box>
+//       )}
 //     </div>
 //   );
 // }
 
-// export default App;
+// function a11yProps(index: number) {
+//   return {
+//     id: `vertical-tab-${index}`,
+//     "aria-controls": `vertical-tabpanel-${index}`,
+//   };
+// }
+
+// export default function VerticalTabs() {
+//   const [value, setValue] = React.useState(0);
+
+//   const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+//     setValue(newValue);
+//   };
+
+//   return (
+// <Container maxWidth="lg" sx={{ mt: 8 }}>
+//   <h1 className='{margin="0px"}'>Hi Welcome</h1>
+//   <h2>John David</h2>
+//   <Grid>
+//     <Box
+//       sx={{
+//         flexGrow: 1,
+//         bgcolor: "background.paper",
+//         display: "flex",
+//         justifyContent: "left",
+//       }}
+//     >
+//       <Card>
+//         <Tabs
+//           orientation="vertical"
+//           variant="scrollable"
+
+//           value={value}
+//           onChange={handleChange}
+//           aria-label="Vertical tabs example"
+//           sx={{ borderRight: 1, borderColor: "divider" }}
+//         >
+//           <div className="text-start">
+
+//             <Tab
+//               style={{ color: "yellow" }}
+//               label="Set Key Info------------------------------------"
+//               {...a11yProps(0)}
+//             />
+//           </div>
+
+//           <Tab label="Manage Job Role-------------------------" {...a11yProps(1)} />
+//           <Tab
+//             label="Staff Settings-----------------------------------"
+//             {...a11yProps(2)}
+//           />
+//           <Tab
+//             label="Define Bank Holiday---------------------------"
+//             {...a11yProps(3)}
+//           />
+//           <Tab
+//             label="Neutral Vendors----------------------------"
+//             {...a11yProps(4)}
+//           />
+//           <Tab
+//             label="Email Notification Settings-----------"
+//             {...a11yProps(5)}
+//           />
+//           <Tab
+//             label="Staff / Client Terms & Conditions----"
+//             {...a11yProps(6)}
+//           />
+//           <Tab label="Agency Staff Agreement-----" {...a11yProps(7)} />
+//           <Tab label="Staff Referral System-----" {...a11yProps(8)} />
+//           <Tab
+//             label="Reset Primary Email and Phone---------"
+//             {...a11yProps(9)}
+//           />
+//           <Tab
+//             label="Choose Your Week Start Day----------"
+//             {...a11yProps(10)}
+//           />
+//           <Tab
+//             label="Admin Document Wallet-----------"
+//             {...a11yProps(11)}
+//           />
+//           <Tab label="Staff Document Wallet---------" {...a11yProps(12)} />
+//           <Tab
+//             label="Electronic Attendance Monitoring------"
+//             {...a11yProps(13)}
+//           />
+//           <Tab label="Festival Day Greetings------" {...a11yProps(14)} />
+//           <Tab
+//             label="Roles and Rights---------------------------"
+//             {...a11yProps(15)}
+//           />
+//         </Tabs>
+//       </Card>
+
+//       <CardContent
+//         style={{ borderColor: "dark", borderStyle: "solid" }}
+//         sx={{
+//           flexGrow: 1,
+//           bgcolor: "background.paper",
+//           display: "flex",
+//           justifyContent: "left",
+//         }}
+//       >
+//         <TabPanel value={value} index={0}>
+//           <Box
+//             component="form"
+//             sx={{
+//               "& .MuiTextField-root": { m: 1, width: "40ch" },
+//             }}
+//             noValidate
+//             autoComplete="off"
+//           >
+//             <div>
+//               <TextField
+//                 error
+//                 id="outlined-error"
+//                 label="Error"
+//                 defaultValue="Hello World"
+//               />
+//               <TextField
+//                 error
+//                 id="outlined-error-helper-text"
+//                 label="Error"
+//                 defaultValue="Hello World"
+//                 helperText="Incorrect entry."
+//               />
+//             </div>
+//             <div>
+//               <TextField
+//                 error
+//                 id="filled-error"
+//                 label="Error"
+//                 defaultValue="Hello World"
+//                 variant="filled"
+//               />
+//               <TextField
+//                 error
+//                 id="filled-error-helper-text"
+//                 label="Error"
+//                 defaultValue="Hello World"
+//                 helperText="Incorrect entry."
+//                 variant="filled"
+//               />
+//             </div>
+//             <div>
+//               <TextField
+//                 error
+//                 id="standard-error"
+//                 label="Error"
+//                 defaultValue="Hello World"
+//                 variant="standard"
+//               />
+//               <TextField
+//                 error
+//                 id="standard-error-helper-text"
+//                 label="Error"
+//                 defaultValue="Hello World"
+//                 helperText="Incorrect entry."
+//                 variant="standard"
+//               />
+//             </div>
+//           </Box>
+//         </TabPanel>
+//         <TabPanel value={value} index={1}>
+//           <Box> Item Two</Box>
+//         </TabPanel>
+//         <TabPanel value={value} index={2}>
+//           Item Three
+//         </TabPanel>
+//         <TabPanel value={value} index={3}>
+//           Item Four
+//         </TabPanel>
+//         <TabPanel value={value} index={4}>
+//           Item Five
+//         </TabPanel>
+//         <TabPanel value={value} index={5}>
+//           Item Six
+//         </TabPanel>
+//         <TabPanel value={value} index={6}>
+//           Item Seven
+//         </TabPanel>
+//         <TabPanel value={value} index={7}>
+//           Item Seven
+//         </TabPanel>
+//         <TabPanel value={value} index={8}>
+//           Item Seven
+//         </TabPanel>
+//         <TabPanel value={value} index={9}>
+//           Item Seven
+//         </TabPanel>
+//         <TabPanel value={value} index={10}>
+//           Item Seven
+//         </TabPanel>
+//         <TabPanel value={value} index={11}>
+//           Item Seven
+//         </TabPanel>
+//         <TabPanel value={value} index={12}>
+//           Item Seven
+//         </TabPanel>
+//         <TabPanel value={value} index={13}>
+//           Item Seven
+//         </TabPanel>
+//         <TabPanel value={value} index={14}>
+//           Item Seven
+//         </TabPanel>
+//         <TabPanel value={value} index={15}>
+//           Item Seven
+//         </TabPanel>
+//       </CardContent>
+//     </Box>
+//   </Grid>
+// </Container>
+//   );
+// }
